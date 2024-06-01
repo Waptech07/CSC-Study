8b876290464834fec00202db5487519f4ee4050c
Waptech <abdulfataiyakub07@gmail.com>
2024-06-01 18:13:32 +0100

    modified navbar for authenticated user, and made some extra changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,62 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { IconButton, Button } from "@mui/material";
+import React, { useContext, useState, useEffect } from "react";
+import { Link, NavLink, useNavigate, useLocation } from "react-router-dom";
+import { AuthContext } from "../context/AuthContext";
+import { Menu, MenuItem, IconButton, Typography, Button } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
+import { motion, AnimatePresence } from "framer-motion";
 
 const NavBar = () => {
+  const { isAuthenticated, user, logout } = useContext(AuthContext);
+  const [anchorEl, setAnchorEl] = useState(null);
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [isFixed, setIsFixed] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
 
   const handleMobileMenuToggle = () => {
     setMobileMenuOpen(!mobileMenuOpen);
   };
 
+  const handleMobileMenuClose = () => {
+    setMobileMenuOpen(false);
+  };
+
+  const isActive = (path) => {
+    return location.pathname === path;
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsFixed(true);
+      } else {
+        setIsFixed(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="bg-white shadow fixed w-full z-10">
-      <div className="container mx-auto px-3 py-5 flex justify-between items-center">
+    <nav
+      className={`bg-white shadow ${
+        isFixed ? "fixed w-full top-0 z-10" : "relative"
+      }`}
+    >
+      <div className="container mx-auto px-4 py-5 flex justify-between items-center">
         <div className="flex items-center space-x-2">
           <Link to="/" className="text-blue-500 font-bold text-2xl">
             {`</>`}
@@ -22,43 +65,97 @@ const NavBar = () => {
             CSC Study
           </Link>
         </div>
-        <div className="hidden md:flex space-x-4">
-          <Link
+        <div className="hidden md:flex space-x-10">
+          <NavLink
+            to="/"
+            className={`text-gray-800 font-bold text-xl uppercase ${
+              isActive("/") && "border-b-[3px] border-blue-500"
+            }`}
+          >
+            Home
+          </NavLink>
+          <NavLink
             to="/courses"
-            className="text-gray-800 font-bold text-xl uppercase"
+            className={`text-gray-800 font-bold text-xl uppercase ${
+              isActive("/courses") && "border-b-[3px] border-blue-500"
+            }`}
           >
             Courses
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to="/about"
-            className="text-gray-800 font-bold text-xl uppercase"
+            className={`text-gray-800 font-bold text-xl uppercase ${
+              isActive("/about") && "border-b-[3px] border-blue-500"
+            }`}
           >
             About
-          </Link>
-          <Link
+          </NavLink>
+          <NavLink
             to="/contact"
-            className="text-blue-500 font-bold text-xl uppercase"
+            className={`text-gray-800 font-bold text-xl uppercase ${
+              isActive("/contact") && "border-b-[3px] border-blue-500"
+            }`}
           >
             Contact
-          </Link>
+          </NavLink>
         </div>
         <div className="hidden md:flex items-center space-x-4">
-          <Button
-            variant="outlined"
-            color="inherit"
-            href="/login"
-            className="text-gray-800 font-bold text-xl uppercase"
-          >
-            Login
-          </Button>
-          <Button
-            variant="contained"
-            href="/register"
-            color="primary"
-            className="text-blue-500 font-bold text-xl uppercase"
-          >
-            Register
-          </Button>
+          {isAuthenticated ? (
+            <div className="flex items-center">
+              <img
+                src={
+                  `http://127.0.0.1:8000${user?.profile_picture}` ||
+                  "default-profile-picture-url"
+                }
+                alt="User"
+                className="w-10 h-10 rounded-full cursor-pointer"
+                onClick={handleProfileMenuOpen}
+              />
+              <Menu
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleMenuClose}
+              >
+                <MenuItem
+                  onClick={() => {
+                    handleMobileMenuClose();
+                    handleMenuClose();
+                    navigate("/profile");
+                  }}
+                >
+                  Profile
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    handleMobileMenuClose();
+                    handleMenuClose();
+                    logout();
+                  }}
+                >
+                  Logout
+                </MenuItem>
+              </Menu>
+            </div>
+          ) : (
+            <>
+              <Button
+                variant="outlined"
+                color="inherit"
+                href="/login"
+                className="text-gray-800 font-bold text-xl uppercase hover:bg-gray-100 hover:text-blue-500 transition duration-300"
+              >
+                Login
+              </Button>
+              <Button
+                variant="contained"
+                href="/register"
+                className="text-blue-500 font-bold text-xl uppercase hover:bg-blue-100 hover:text-blue-600 transition duration-300"
+              >
+                Register
+              </Button>
+            </>
+          )}
         </div>
         <div className="md:hidden flex items-center">
           <IconButton onClick={handleMobileMenuToggle}>
@@ -66,40 +163,112 @@ const NavBar = () => {
           </IconButton>
         </div>
       </div>
-      {mobileMenuOpen && (
-        <div className="md:hidden bg-white shadow-md">
-          <Link
-            to="/courses"
-            className="block px-4 py-2 text-gray-800 font-bold text-xl uppercase"
+      <AnimatePresence>
+        {mobileMenuOpen && (
+          <motion.div
+            initial={{ opacity: 0, x: "-100%" }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: "-100%" }}
+            className="md:hidden fixed inset-0 bg-white shadow-md z-20"
           >
-            Courses
-          </Link>
-          <Link
-            to="/about"
-            className="block px-4 py-2 text-gray-800 font-bold text-xl uppercase"
-          >
-            About
-          </Link>
-          <Link
-            to="/contact"
-            className="block px-4 py-2 text-blue-500 font-bold text-xl uppercase"
-          >
-            Contact
-          </Link>
-          <Link
-            to="/login"
-            className="block px-4 py-2 text-gray-800 font-bold text-xl uppercase"
-          >
-            Login
-          </Link>
-          <Link
-            to="/register"
-            className="block px-4 py-2 text-blue-500 font-bold text-xl uppercase"
-          >
-            Register
-          </Link>
-        </div>
-      )}
+            <div className="container mx-auto px-4 py-5 flex flex-col justify-start items-start h-full w-full">
+              <div className="flex justify-between text-center mb-6">
+                <div className="flex items-center space-x-2">
+                  <Link to="/" className="text-blue-500 font-bold text-2xl">
+                    {`</>`}
+                  </Link>
+                  <Link to="/" className="text-3xl font-bold text-gray-800">
+                    CSC Study
+                  </Link>
+                </div>
+                <div className="flex fixed right-3">
+                  <IconButton onClick={handleMobileMenuToggle}>
+                    <CloseIcon />
+                  </IconButton>
+                </div>
+              </div>
+              <NavLink
+                to="/"
+                onClick={handleMobileMenuToggle}
+                className={`block py-2 text-gray-800 font-semibold text-xl capitalize pl-5 w-full hover:bg-blue-500 hover:text-white transition duration-300 ${
+                  isActive("/") && "bg-blue-500 text-white hover:bg-blue-600"
+                }`}
+              >
+                Home
+              </NavLink>
+              <NavLink
+                to="/courses"
+                onClick={handleMobileMenuToggle}
+                className={`block py-2 text-gray-800 font-semibold text-xl capitalize pl-5 w-full hover:bg-blue-500 hover:text-white transition duration-300 ${
+                  isActive("/courses") &&
+                  "bg-blue-500 text-white hover:bg-blue-600"
+                }`}
+              >
+                Courses
+              </NavLink>
+              <NavLink
+                onClick={handleMobileMenuToggle}
+                to="/about"
+                className={`block py-2 text-gray-800 font-semibold text-xl capitalize pl-5 w-full hover:bg-blue-500 hover:text-white transition duration-300 ${
+                  isActive("/about") &&
+                  "bg-blue-500 text-white hover:bg-blue-600"
+                }`}
+              >
+                About
+              </NavLink>
+              <NavLink
+                onClick={handleMobileMenuToggle}
+                to="/contact"
+                className={`block py-2 text-gray-800 font-semibold text-xl capitalize pl-5 w-full hover:bg-blue-500 hover:text-white transition duration-300 ${
+                  isActive("/contact") &&
+                  "bg-blue-500 text-white hover:bg-blue-600"
+                }`}
+              >
+                Contact
+              </NavLink>
+              {isAuthenticated ? (
+                <>
+                  <NavLink
+                    onClick={handleProfileMenuOpen}
+                    className={`flex items-center justify-between py-2 text-gray-800 font-semibold text-xl capitalize pl-5 w-full hover:bg-blue-500 hover:text-white transition duration-300 ${
+                      isActive("/profile") &&
+                      "bg-blue-500 text-white hover:bg-blue-600"
+                    }`}
+                  >
+                    Profile
+                    <IconButton className="pr-40">
+                      <MenuIcon />
+                    </IconButton>
+                  </NavLink>
+                </>
+              ) : (
+                <>
+                  <NavLink
+                    to="/login"
+                    className={`block py-2 text-gray-800 font-semibold text-xl capitalize pl-5 w-full hover:bg-blue-500 hover:text-white transition duration-300 ${
+                      isActive("/login") &&
+                      "bg-blue-500 text-white hover:bg-blue-600"
+                    }`}
+                    onClick={handleMobileMenuToggle}
+                  >
+                    Login
+                  </NavLink>
+                  <NavLink
+                    to="/register"
+                    className={`block py-2 text-gray-800 font-semibold text-xl capitalize pl-5 w-full hover:bg-blue-500 hover:text-white transition duration-300 ${
+                      isActive("/register") &&
+                      "bg-blue-500 text-white hover:bg-blue-600"
+                    }`}
+                    onClick={handleMobileMenuToggle}
+                  >
+                    Register
+                  </NavLink>
+                </>
+              )}
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </nav>
   );
 };
