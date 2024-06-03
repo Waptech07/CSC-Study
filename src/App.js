c1494e5f92ea4ee004bf3d3f3aec3fd6a77c2827
Waptech <abdulfataiyakub07@gmail.com>
2024-06-03 23:26:48 +0100

    Added nested routes for profile page

--- src/App.js
+++ src/App.js
@@ -12,18 +12,33 @@ import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import NotFound from './components/NotFound';
 import Home from './pages/Home';
+import ProfilePage from './pages/profile/Profile';
+import MyProfile from './components/Profile/MyProfile';
+import AllCourses from './components/Profile/AllCourses';
+import ActiveCourses from './components/Profile/ActiveCourses';
+import CompletedCourses from './components/Profile/CompletedCourses';
+import Settings from './components/Profile/Settings';
+import PurchaseHistory from './components/Profile/PurchaseHistory';
 
 const App = () => {
   return (
     <Router>
       <AuthProvider>
-        <ToastContainer/>
+        <ToastContainer />
         <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path='*' element={<NotFound/>}/>
+          <Route path="/profile" element={<ProfilePage />}>
+            <Route path="details" element={<MyProfile />} />
+            <Route path="all-courses" element={<AllCourses />} />
+            <Route path="active-courses" element={<ActiveCourses />} />
+            <Route path="completed-courses" element={<CompletedCourses />} />
+            <Route path="purchase-history" element={<PurchaseHistory />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </AuthProvider>
     </Router>
