0a5852f2db89c587f7295e4b293b62f97936ece8
Waptech <abdulfataiyakub07@gmail.com>
2024-06-03 12:17:15 +0100

    used routes, AuthProvider, react toastify and swiper

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import "swiper/css";
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import NavBar from './components/Navbar';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import NotFound from './components/NotFound';
+import Home from './pages/Home';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <AuthProvider>
+        <ToastContainer/>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path='*' element={<NotFound/>}/>
+        </Routes>
+      </AuthProvider>
+    </Router>
   );
-}
+};
 
 export default App;
