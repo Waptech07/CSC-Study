e492310a770df086bad413609db01a3906f478f8
Waptech <abdulfataiyakub07@gmail.com>
2024-06-04 15:23:21 +0100

    added forgot password and reset password routes, also added a gray background

--- src/App.js
+++ src/App.js
@@ -19,29 +19,35 @@ import ActiveCourses from './components/Profile/ActiveCourses';
 import CompletedCourses from './components/Profile/CompletedCourses';
 import Settings from './components/Profile/Settings';
 import PurchaseHistory from './components/Profile/PurchaseHistory';
+import ForgotPassword from './pages/auth/ForgotPassword';
+import ResetPassword from './pages/auth/ResetPassword';
 
 const App = () => {
   return (
-    <Router>
-      <AuthProvider>
-        <ToastContainer />
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<ProfilePage />}>
-            <Route path="details" element={<MyProfile />} />
-            <Route path="all-courses" element={<AllCourses />} />
-            <Route path="active-courses" element={<ActiveCourses />} />
-            <Route path="completed-courses" element={<CompletedCourses />} />
-            <Route path="purchase-history" element={<PurchaseHistory />} />
-            <Route path="settings" element={<Settings />} />
-          </Route>
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </AuthProvider>
-    </Router>
+    <div className='h-[100vh] bg-gray-100'>
+      <Router>
+        <AuthProvider>
+          <ToastContainer />
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/reset-password/:uid/:token" element={<ResetPassword />} />
+            <Route path="/profile" element={<ProfilePage />}>
+              <Route path="details" element={<MyProfile />} />
+              <Route path="all-courses" element={<AllCourses />} />
+              <Route path="active-courses" element={<ActiveCourses />} />
+              <Route path="completed-courses" element={<CompletedCourses />} />
+              <Route path="purchase-history" element={<PurchaseHistory />} />
+              <Route path="settings" element={<Settings />} />
+            </Route>
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </AuthProvider>
+      </Router>
+    </div>
   );
 };
 
