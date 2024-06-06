332970212639243b8ed8581a4228a9fb3d7bbde7
Waptech <abdulfataiyakub07@gmail.com>
2024-06-06 10:03:32 +0100

    added lazy loading to image

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import notFound from '../assets/not-found.png'; // Ensure you have an illustration image
+import React from "react";
+import { Link } from "react-router-dom";
+import notFound from "../assets/not-found.png"; // Ensure you have an illustration image
 
 const NotFound = () => {
   return (
@@ -10,9 +10,12 @@ const NotFound = () => {
           src={notFound}
           alt="Not Found"
           className="mx-auto"
+          loading="lazy"
         />
         <h1 className="text-4xl font-bold mb-4">Page Not Found</h1>
-        <p className="text-lg mb-8">The page you are looking for doesn't exist or has been moved.</p>
+        <p className="text-lg mb-8">
+          The page you are looking for doesn't exist or has been moved.
+        </p>
         <Link to="/" className="text-blue-500 font-bold text-xl">
           Go Back Home
         </Link>
