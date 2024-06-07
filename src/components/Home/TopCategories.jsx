412741d20fc96de172fd8f5c2e016f4256fc40f9
Waptech <abdulfataiyakub07@gmail.com>
2024-06-07 11:43:23 +0100

    successfully fetched categories for endpoint & made few chnages

--- src/components/Home/TopCategories.jsx
+++ src/components/Home/TopCategories.jsx
@@ -1,31 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay } from "swiper/modules";
-import {
-  FaHtml5,
-  FaJsSquare,
-  FaPython,
-  FaJava,
-  FaNodeJs,
-  FaReact,
-} from "react-icons/fa";
+import { getCategories } from "../../services/coursesApi";
 
 const TopCategories = () => {
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const data = await getCategories();
+        setCategories(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <>
-      <section className="mb-16">
-        <h2 className="text-3xl text-center font-bold mb-4 uppercase">
+      <section className="lg:mb-16 lg:py-5 py-16 bg-white">
+        <h2 className="lg:text-3xl text-2xl text-center font-bold mb-4 uppercase">
           Browse Courses by Top Categories
         </h2>
-        <div className="flex items-center justify-center p-10">
+        <div className="flex items-center justify-between p-10">
           <Swiper
             modules={[Autoplay, Pagination]}
             spaceBetween={20}
             slidesPerView={1}
             autoplay={{
               delay: 2500,
-              disableOnInteraction: false,
+              disableOnInteraction: true,
             }}
             breakpoints={{
               640: {
@@ -47,58 +62,26 @@ const TopCategories = () => {
             }}
             pagination={{ clickable: true }}
           >
-            {[
-              {
-                icon: FaHtml5,
-                color: "orange-500",
-                title: "Web",
-                courses: 4,
-              },
-              {
-                icon: FaJsSquare,
-                color: "yellow-400",
-                title: "Frontend",
-                courses: 4,
-              },
-              {
-                icon: FaNodeJs,
-                color: "green-500",
-                title: "Backend",
-                courses: 4,
-              },
-              {
-                icon: FaReact,
-                color: "blue-500",
-                title: "Full Stack",
-                courses: 4,
-              },
-              {
-                icon: FaPython,
-                color: "blue-700",
-                title: "Python",
-                courses: 4,
-              },
-              {
-                icon: FaJsSquare,
-                color: "yellow-400",
-                title: "JavaScript",
-                courses: 4,
-              },
-              {
-                icon: FaJava,
-                color: "red-500",
-                title: "Java",
-                courses: 4,
-              },
-            ].map(({ icon: Icon, color, title, courses }, index) => (
-              <SwiperSlide key={index}>
-                <div
-                  className={`bg-white border border-${color} hover:shadow-lg transition-shadow duration-300 hover:shadow-${color} p-6 mb-10 flex flex-col justify-center items-center`}
-                >
-                  <Icon className={`text-${color} text-5xl mb-4`} />
-                  <h3 className="text-xl font-bold">{title}</h3>
-                  <p className="text-base text-gray-500">{courses} Courses</p>
-                </div>
+            {categories.map(({ id, name, image, course_count }) => (
+              <SwiperSlide key={id}>
+                <Link to={`/category/${id}`}>
+                  <div className="border border-blue-300 hover:shadow-lg transition-shadow duration-300 hover:shadow-blue-300 py-6 px-2 mb-10">
+                    <div className="flex flex-col justify-center items-center">
+                      <img
+                        src={
+                          `http://127.0.0.1:8000${image}` ||
+                          "https://via.placeholder.com/150"
+                        }
+                        alt={name}
+                        className="w-20 h-20"
+                      />
+                      <h3 className="text-lg font-bold">{name}</h3>
+                      <p className="text-base text-gray-500">
+                        {course_count} Courses
+                      </p>
+                    </div>
+                  </div>
+                </Link>
               </SwiperSlide>
             ))}
           </Swiper>
