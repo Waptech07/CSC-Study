938c0995d45559b0636c1ecd4801323a23f0270b
Waptech <abdulfataiyakub07@gmail.com>
2024-06-07 11:51:31 +0100

    created page to view the instructor of a course

---
+++ src/pages/profile/ViewInstructor.jsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import { NavLink, Outlet, useLocation, useParams } from "react-router-dom";
+import { getInstructorDetails } from "../../services/coursesApi";
+
+const ViewInstructor = () => {
+  const { id } = useParams();
+  const [instructor, setInstructor] = useState(null);
+  const location = useLocation();
+  const isActive = (path) => {
+    return location.pathname === path;
+  };
+
+  useEffect(() => {
+    const fetchInstructor = async () => {
+      try {
+        const fetchedInstructor = await getInstructorDetails(id);
+        setInstructor(fetchedInstructor);
+      } catch (error) {
+        console.error("Error fetching instructor details:", error);
+      }
+    };
+
+    fetchInstructor();
+  }, [id]);
+
+  if (!instructor) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <section className="bg-gray-100">
+      <div>
+        <div className="flex flex-wrap lg:flex-nowrap lg:p-10 p-5 items-start">
+          <div className="flex flex-col justify-center items-center w-full lg:w-1/3 bg-white shadow-md rounded-lg py-10 gap-5">
+            <img
+              src={
+                instructor.user.profile_picture
+                  ? `http://127.0.0.1:8000${instructor.user.profile_picture}`
+                  : "https://via.placeholder.com/150"
+              }
+              className="rounded-full w-60 h-60"
+              alt="profile-image"
+            />
+            <div className="flex flex-col justify-center items-center">
+              <p className="text-black font-bold text-3xl">
+                {instructor.user.name}
+              </p>
+              <p className="text-gray-500 font-semibold text-lg">
+                {instructor.user.profession}
+              </p>
+            </div>
+            <hr className="w-10/12 h-px bg-gray-400 border-0 px-10 my-5" />
+            <div className="px-10">
+              <h2 className="text-gray-800 font-bold text-lg mb-3">About Me</h2>
+              <p className="text-gray-500 font-medium text-base">
+                {instructor.user.bio}
+              </p>
+            </div>
+            <hr className="w-10/12 h-px bg-gray-400 border-0 px-10 my-5" />
+            <div className="px-10">
+              <h2 className="text-gray-800 font-bold text-lg mb-3">
+                Biography
+              </h2>
+              <p className="text-gray-500 font-medium text-base">
+                {instructor.biography}
+              </p>
+            </div>
+          </div>
+          <div className="flex-1">
+            <div className="flex gap-5 pb-2 pl-5">
+              {[
+                { to: `/instructor/${instructor.id}`, label: "Courses" },
+                { to: `/instructor/${instructor.id}/details`, label: "Details" },
+              ].map((link) => (
+                <NavLink
+                  key={link.to}
+                  to={link.to}
+                  className={`text-gray-800 text-sm lg:text-base font-semibold py-2 ${
+                    isActive(link.to) && "border-b-4 border-blue-500 pb-1"
+                  }`}
+                >
+                  {link.label}
+                </NavLink>
+              ))}
+            </div>
+            <Outlet />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ViewInstructor;
