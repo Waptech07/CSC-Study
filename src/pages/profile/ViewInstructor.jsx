389f5ee55c08017c1f0a7ca275a94e2dc2b3d12d
Waptech <abdulfataiyakub07@gmail.com>
2024-06-08 01:29:05 +0100

    added ? for instructor and change url route for outlet

--- src/pages/profile/ViewInstructor.jsx
+++ src/pages/profile/ViewInstructor.jsx
@@ -35,8 +35,8 @@ const ViewInstructor = () => {
           <div className="flex flex-col justify-center items-center w-full lg:w-1/3 bg-white shadow-md rounded-lg py-10 gap-5">
             <img
               src={
-                instructor.user.profile_picture
-                  ? `http://127.0.0.1:8000${instructor.user.profile_picture}`
+                instructor?.user.profile_picture
+                  ? `http://127.0.0.1:8000${instructor?.user.profile_picture}`
                   : "https://via.placeholder.com/150"
               }
               className="rounded-full w-60 h-60"
@@ -44,17 +44,17 @@ const ViewInstructor = () => {
             />
             <div className="flex flex-col justify-center items-center">
               <p className="text-black font-bold text-3xl">
-                {instructor.user.name}
+                {instructor?.user.name}
               </p>
               <p className="text-gray-500 font-semibold text-lg">
-                {instructor.user.profession}
+                {instructor?.user.profession}
               </p>
             </div>
             <hr className="w-10/12 h-px bg-gray-400 border-0 px-10 my-5" />
             <div className="px-10">
               <h2 className="text-gray-800 font-bold text-lg mb-3">About Me</h2>
               <p className="text-gray-500 font-medium text-base">
-                {instructor.user.bio}
+                {instructor?.user.bio}
               </p>
             </div>
             <hr className="w-10/12 h-px bg-gray-400 border-0 px-10 my-5" />
@@ -63,15 +63,15 @@ const ViewInstructor = () => {
                 Biography
               </h2>
               <p className="text-gray-500 font-medium text-base">
-                {instructor.biography}
+                {instructor?.biography}
               </p>
             </div>
           </div>
           <div className="flex-1">
             <div className="flex gap-5 pb-2 pl-5">
               {[
-                { to: `/instructor/${instructor.id}`, label: "Courses" },
-                { to: `/instructor/${instructor.id}/details`, label: "Details" },
+                { to: `/instructor/${instructor?.id}`, label: "Courses" },
+                { to: `/instructor/${instructor?.id}/details`, label: "Details" },
               ].map((link) => (
                 <NavLink
                   key={link.to}
