28dbb047215d2f335dd03576519b18c72dce17de
Waptech <abdulfataiyakub07@gmail.com>
2024-06-09 10:24:03 +0100

    registered all routes and made few changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import "swiper/css";
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import 'swiper/css/grid';
 import NavBar from './components/Navbar';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
@@ -21,8 +22,25 @@ import Settings from './components/Profile/Settings';
 import PurchaseHistory from './components/Profile/PurchaseHistory';
 import ForgotPassword from './pages/auth/ForgotPassword';
 import ResetPassword from './pages/auth/ResetPassword';
+import InstructorProfile from './pages/profile/InstructorProfile';
+import InstructorCourses from './components/Profile/Instructor/InstructorCourses';
+import CourseDetailsPage from './pages/courses/CourseDetailsPage';
+import CoursesPage from './pages/courses/CoursesPage';
+import AboutUsPage from './pages/AboutUs';
+import ContactUsPage from './pages/ContactUs';
+import Footer from './components/Footer';
+import ProtectedRoute from './components/ProtectedRoutes';
+import ViewInstructor from './pages/profile/ViewInstructor';
+import CategoryCoursesPage from './pages/courses/CategoryCoursesPage';
+import ViewInstructorCourses from './components/Profile/View/ViewInstructorCourses';
+import ViewInstructorDetails from './components/Profile/View/ViewInstructorDetails';
+import CourseOverview from './components/Courses/CourseOverview';
+import CourseLessons from './components/Courses/CourseLessons';
+import LessonDetails from './pages/courses/LessonDetails';
+import InstructorSettings from './components/Profile/Instructor/InstructorSettings';
 
 const App = () => {
+
   return (
     <div className='h-[100vh] bg-gray-100'>
       <Router>
@@ -31,20 +49,43 @@ const App = () => {
           <NavBar />
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutUsPage />} />
+            <Route path="/contact" element={<ContactUsPage />} />
+
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path="/reset-password/:uid/:token" element={<ResetPassword />} />
-            <Route path="/profile" element={<ProfilePage />}>
-              <Route path="details" element={<MyProfile />} />
+
+            <Route path="/profile" element={<ProtectedRoute><ProfilePage /></ProtectedRoute>}>
+              <Route path="" element={<MyProfile />} />
               <Route path="all-courses" element={<AllCourses />} />
               <Route path="active-courses" element={<ActiveCourses />} />
               <Route path="completed-courses" element={<CompletedCourses />} />
               <Route path="purchase-history" element={<PurchaseHistory />} />
               <Route path="settings" element={<Settings />} />
             </Route>
+
+            <Route path="/my-profile" element={<ProtectedRoute><InstructorProfile /></ProtectedRoute>}>
+              <Route path="" element={<InstructorCourses />} />
+              <Route path="settings" element={<InstructorSettings />} />
+            </Route>
+
+            <Route path="courses/:courseId" element={<CourseDetailsPage />}>
+              <Route path="" element={<CourseOverview />} />
+              <Route path="lessons" element={<CourseLessons />} />
+            </Route>
+            <Route path="/courses" element={<CoursesPage />} />
+            <Route path="/category/:categoryId" element={<CategoryCoursesPage />} />
+            <Route path="/courses/:courseId/lessons/:lessonId" element={<LessonDetails/>} />
+
+            <Route path="/instructor/:id" element={<ViewInstructor />} >
+              <Route path="" element={<ViewInstructorCourses />} />
+              <Route path="details" element={<ViewInstructorDetails />} />
+            </Route>
             <Route path='*' element={<NotFound />} />
           </Routes>
+          <Footer />
         </AuthProvider>
       </Router>
     </div>
