604ed7cdab928d2ccb187dc1aede762e8018ca6f
Waptech <abdulfataiyakub07@gmail.com>
2024-06-09 10:16:46 +0100

    made few changes by fetching instructors from database

--- src/components/Home/OurBestInstructors.jsx
+++ src/components/Home/OurBestInstructors.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
-import instructorImage from "../../assets/instructor.jpg";
+import { getInstructors } from "../../services/coursesApi";
+import Loading from "../Loading";
 
 const OurBestInstructors = () => {
+  const [instructors, setInstructors] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchInstructors = async () => {
+      try {
+        const data = await getInstructors();
+        setInstructors(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching Instructors:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchInstructors();
+  }, []);
+
+  if (loading) {
+    <Loading />;
+  }
+
   return (
     <>
       <section className="mb-16 p-10 md:p-20">
         <h2 className="text-4xl md:text-5xl font-bold mb-4 text-center">
           Meet Our Best Instructors
         </h2>
+        <div className="md:px-0 px-40">
+
         <Swiper
           modules={[Pagination]}
           spaceBetween={20}
@@ -18,77 +43,40 @@ const OurBestInstructors = () => {
             768: {
               slidesPerView: 2,
               spaceBetween: 20,
-            },
-            1024: {
-              slidesPerView: 3,
-              spaceBetween: 30,
-            },
-            1280: {
-              slidesPerView: 4,
-              spaceBetween: 40,
-            },
-          }}
-          pagination={{ clickable: true }}
-        >
-          {[
-            {
-              name: "Victory Elijah",
-              title: "Web Developer",
-              image: instructorImage,
-            },
-            {
-              name: "Sarah Johnson",
-              title: "Frontend Developer",
-              image: instructorImage,
-            },
-            {
-              name: "Michael Brown",
-              title: "Backend Developer",
-              image: instructorImage,
-            },
-            {
-              name: "Emily Davis",
-              title: "Full Stack Developer",
-              image: instructorImage,
-            },
-            {
-              name: "Daniel Wilson",
-              title: "Data Scientist",
-              image: instructorImage,
-            },
-            {
-              name: "Sophia Miller",
-              title: "UX/UI Designer",
-              image: instructorImage,
-            },
-            {
-              name: "James Anderson",
-              title: "DevOps Engineer",
-              image: instructorImage,
-            },
-            {
-              name: "Olivia Martinez",
-              title: "Mobile Developer",
-              image: instructorImage,
-            },
-          ].map((instructor, index) => (
+              },
+              1024: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+                },
+                1280: {
+                  slidesPerView: 4,
+                  spaceBetween: 40,
+                  },
+                  }}
+                  pagination={{ clickable: true }}
+                  >
+          {instructors.map((instructor, index) => (
             <SwiperSlide key={index}>
               <div className="bg-white border my-10 hover:shadow-md">
                 <img
-                  src={instructor.image}
-                  alt="Instructor"
-                  className="w-full"
-                />
-                <div className="p-4">
+                  src={
+                    `http://127.0.0.1:8000${instructor.user.profile_picture}` ||
+                    "https://via.placeholder.com/150"
+                    }
+                    alt="Instructor"
+                    className="w-full h-60"
+                    />
+                <div className="p-4 h-24">
                   <h3 className="text-lg md:text-xl font-bold mb-2">
-                    {instructor.name}
+                    {instructor.user.name}
                   </h3>
-                  <p>{instructor.title}</p>
+                  <p>{instructor.user.profession}</p>
                 </div>
               </div>
             </SwiperSlide>
           ))}
         </Swiper>
+        </div>
       </section>
     </>
   );
