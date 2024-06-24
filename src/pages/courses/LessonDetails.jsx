e548ee7b195b8c4860325e4cf01d7fed1f2f092c
Waptech <abdulfataiyakub07@gmail.com>
2024-06-24 11:55:28 +0100

    made some changes & restyled the lesson details page

--- src/pages/courses/LessonDetails.jsx
+++ src/pages/courses/LessonDetails.jsx
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import {
   getLessonDetails,
   getCourseDetails,
   completeLesson,
-} from "../../services/coursesApi"; // Import CompleteLesson from coursesApi
+} from "../../services/coursesApi";
 import Loading from "../../components/Loading";
 import { BigPlayButton, LoadingSpinner, Player } from "video-react";
-import "video-react/dist/video-react.css"; // import video-react styles
+import "video-react/dist/video-react.css";
+import { FaSignOutAlt } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const LessonDetails = () => {
-  const { courseId, lessonId, courseSlug, lessonSlug } = useParams();
+  const { courseSlug, lessonSlug } = useParams();
   const [lesson, setLesson] = useState(null);
   const [course, setCourse] = useState(null);
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchLessonAndCourseDetails = async () => {
@@ -32,17 +35,14 @@ const LessonDetails = () => {
     };
 
     fetchLessonAndCourseDetails();
-  }, [courseId, lessonId]);
+  }, [courseSlug, lessonSlug]);
 
   const handleCompleteLesson = async () => {
     try {
-      const response = await completeLesson(courseId, lessonId);
-      // Call CompleteLesson with courseId and lessonId
+      const response = await completeLesson(course.id, lesson.id);
       console.log("Lesson completed successfully:", response);
-      // You might want to update the UI or state based on the response, e.g., set a completion status
     } catch (error) {
       console.error("Error completing lesson:", error);
-      // Handle errors, e.g., show an error message to the user
     }
   };
 
@@ -82,21 +82,32 @@ const LessonDetails = () => {
   }
 
   return (
-    <section className="bg-gray-100">
-      <div className="flex lg:flex-row flex-col justify-center items-start lg:space-x-8">
-        <div className="lg:max-w-3xl w-full bg-white p-8 lg:rounded-lg lg:shadow-md">
+    <motion.section
+      className="bg-gray-100 min-h-screen p-6"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <div className="container mx-auto flex flex-col lg:flex-row gap-8">
+        <button
+          onClick={() => navigate(-1)}
+          className="flex items-center text-blue-600 hover:text-blue-800 mb-4"
+        >
+          <FaSignOutAlt className="text-xl mr-2" />
+          Back
+        </button>
+        <motion.div
+          className="flex-1 bg-white p-6 rounded-lg shadow-md"
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
           {lesson.video && (
-            <div className="mb-8 relative">
-              <h2 className="lg:text-2xl text-xl text-gray-700 font-semibold">
+            <div className="mb-6">
+              <h2 className="text-xl text-gray-700 font-semibold mb-4">
                 Course - {course.title}
               </h2>
-              <Player
-                poster={lesson.images}
-                src={`${lesson.video}`}
-                fluid={true}
-                width={720}
-                height={405}
-              >
+              <Player poster={lesson.images} src={lesson.video} fluid>
                 <LoadingSpinner />
                 <BigPlayButton position="center" />
               </Player>
@@ -105,15 +116,20 @@ const LessonDetails = () => {
           <h1 className="text-3xl font-bold mb-4 text-gray-900">
             {lesson.title}
           </h1>
-          <p className="text-lg text-gray-700 mb-8">{lesson.content}</p>
+          <p className="text-lg text-gray-700 mb-6">{lesson.content}</p>
           {resources.length > 0 && (
-            <div className="mb-8">
+            <div className="mb-6">
               <h2 className="text-2xl font-semibold mb-4 text-gray-900">
                 Additional Resources
               </h2>
-              <ul className="list-disc list-inside">
+              <ul className="list-disc list-inside mb-4">
                 {resources.map((resource, index) => (
-                  <li key={index} className="mb-2">
+                  <motion.li
+                    key={index}
+                    className="mb-2"
+                    whileHover={{ scale: 1.05 }}
+                    transition={{ duration: 0.3 }}
+                  >
                     <a
                       href={resource.url}
                       target="_blank"
@@ -122,44 +138,49 @@ const LessonDetails = () => {
                     >
                       {resource.type}
                     </a>
-                  </li>
+                  </motion.li>
                 ))}
               </ul>
               <button
                 onClick={handleCompleteLesson}
-                className="px-2 py-4 rounded-md bg-green-700 hover:bg-green-600 text-white font-bold"
+                className="px-4 py-2 rounded-md bg-green-600 hover:bg-green-500 text-white font-bold transition duration-300"
               >
                 Complete Lesson
               </button>
             </div>
           )}
-        </div>
-        <div className="lg:w-64 w-full bg-white p-3 lg:rounded-lg lg:shadow-md">
-          <h2 className="text-2xl font-semibold mb-4 text-gray-900 pl-3">
-            Lessons
-          </h2>
+        </motion.div>
+        <motion.div
+          className="lg:w-1/3 w-full bg-white p-6 rounded-lg shadow-md"
+          initial={{ x: 100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <h2 className="text-2xl font-semibold mb-4 text-gray-900">Lessons</h2>
           <ul>
-            {course.lessons.map((lesson) => (
-              <li
-                key={lesson.id}
-                className={`mb-4 rounded ${
-                  lesson.id === parseInt(lessonId)
+            {course.lessons.map((lessonItem) => (
+              <motion.li
+                key={lessonItem.id}
+                className={`mb-4 rounded-md ${
+                  lessonItem.slug === lessonSlug
                     ? "bg-blue-500 text-white"
-                    : "text-blue-800"
+                    : "text-blue-800 hover:bg-gray-200"
                 }`}
+                whileHover={{ scale: 1.05 }}
+                transition={{ duration: 0.3 }}
               >
                 <Link
-                  to={`/courses/${courseId}/lessons/${lesson.id}`}
-                  className="block px-6 py-2 hover:text-black hover:bg-gray-300"
+                  to={`/courses/${courseSlug}/lessons/${lessonItem.slug}`}
+                  className="block px-4 py-2 transition duration-300"
                 >
-                  {lesson.title}
+                  {lessonItem.title}
                 </Link>
-              </li>
+              </motion.li>
             ))}
           </ul>
-        </div>
+        </motion.div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
