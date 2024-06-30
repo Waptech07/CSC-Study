9d6c6b52fe123be03acfd973f19fe3b90c734efd
Waptech <abdulfataiyakub07@gmail.com>
2024-06-30 18:43:45 +0100

    added the blog navbar component and their props

--- src/blog/BlogLayout.jsx
+++ src/blog/BlogLayout.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
+import BlogNavbar from "./components/BlogNavbar";
 
 const BlogLayout = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [searchQuery, setSearchQuery] = useState("");
+
   return (
     <>
       <div className="min-h-screen bg-gray-100">
+        <BlogNavbar
+          setSelectedCategory={setSelectedCategory}
+          setSearchQuery={setSearchQuery}
+        />
         <main className="mx-auto p-8">
-          <Outlet />
+          <Outlet context={{ selectedCategory, searchQuery }} />
         </main>
       </div>
     </>
